290ef9e4aa40ac10a699323f0939805980bdfe08
Trent <trentengelman0@gmail.com>
2020-10-03 19:46:54 -0700

    US22 - Task45 - Trent learns PyTest

--- server/server.py
+++ server/server.py
@@ -9,27 +9,27 @@ def graph():
     content = request.json
     slope = content["slope"]
     y_intercept = content["yIntercept"]
-    x = getX(100)
-    y = getY(x, slope, y_intercept)
-    createFig(x, y)
-    img = {"image": serializeFig()}
+    x = get_x(100)
+    y = get_y(x, slope, y_intercept)
+    create_fig(x, y)
+    img = {"image": serialize_fig()}
     return jsonify(img)
 
-def getX(range: int):
-    return np.arange(100)
+def get_x(range: int):
+    return np.arange(range)
 
-def getY(x, slope: int, y_intercept: int):
+def get_y(x, slope: int, y_intercept: int):
     calculate = lambda x: x * slope + y_intercept
     return calculate(x)
 
-def createFig(x, y):
+def create_fig(x, y):
     plt.plot(x, y)
     plt.xlabel('x')
     plt.ylabel('y')
     plt.axis([0, 100, 0, 100])
     plt.savefig('./temp/temp.png')
 
-def serializeFig() -> str:
+def serialize_fig() -> str:
     with open("./temp/temp.png", "rb") as image_file:
         encoding = base64.b64encode(image_file.read())
     return str(encoding)
